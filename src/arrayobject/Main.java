ead89fe66de17554f1e2715b8f3333d6fe57b3f7
reshabh-k <kumarres@sheridancollege.ca>
2022-05-25 10:04:28 -0400

    Version-1 code

---
+++ src/arrayobject/Main.java
@@ -0,0 +1,29 @@
+package arrayobject;
+
+import arrayobject.Student.Student;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        Student s2 = new Student();
+        s2.setName("Sherry");
+        s2.setAge(18);
+
+        Student s3 = new Student();
+        s3.setName("Rajvir");
+        s3.setAge(19);
+
+        Student[] students = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+
+    }
+}
---
+++ src/arrayobject/Student/Student.java
@@ -0,0 +1,40 @@
+package arrayobject.Student;
+
+public class Student {
+
+    private String name = "Reshabh";
+    private int age = 19;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return ("The name of the student is: " + getName() + " and the age of the student is: " + getAge());
+    }
+
+}
